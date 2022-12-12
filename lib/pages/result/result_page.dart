ce197979160c6a79eda57ac0c4c3334f48d27bb1
Theus <matheusth.dev@gmail.com>
2022-12-12 00:44:57 -0300

    feat: result page button

--- lib/pages/result/result_page.dart
+++ lib/pages/result/result_page.dart
@@ -1,10 +1,45 @@
+import 'package:ask/components/button.dart';
+import 'package:ask/constants.dart';
+import 'package:ask/styles/global_styles.dart';
 import 'package:flutter/material.dart';
 
 class ResultPage extends StatelessWidget {
   const ResultPage({super.key});
 
+  void goTo(String path) {
+    globalNavigatorKey.currentState!.pushNamed(path);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          padding: marginPaddingAll(20),
+          color: darkRed,
+          child: Center(
+            child: Column(
+              children: [
+                Button(
+                  text: "Sair",
+                  onPressed: () => goTo("/"),
+                  width: 200,
+                  height: 50,
+                ),
+                Container(
+                  margin: marginPaddingVertical(10),
+                  child: Button(
+                    text: "Jogar Novamente",
+                    onPressed: () => goTo("/home"),
+                    width: 200,
+                    height: 50,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
